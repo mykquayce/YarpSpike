953412a5bf03313c322acd247c15aa5570d8f471
bob h <mail@bob.codes>
2021-11-14 22:10:24 +0000

    attempt1
    ...works but subsequent requests for styles and scripts fail

--- YarpSpike.WebApi/Program.cs
+++ YarpSpike.WebApi/Program.cs
@@ -5,9 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddReverseProxy()
+	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapReverseProxy();
+
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -16,28 +21,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-	var forecast = Enumerable.Range(1, 5).Select(index =>
-	   new WeatherForecast
-	   (
-		   DateTime.Now.AddDays(index),
-		   Random.Shared.Next(-20, 55),
-		   summaries[Random.Shared.Next(summaries.Length)]
-	   ))
-		.ToArray();
-	return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
